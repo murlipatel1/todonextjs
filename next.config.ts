ce3d1dc03a7beec87bb9435a89254f814c87e7a3
Murli Patel <patelmurli15@gmail.com>
2025-01-16 17:50:43 +0530

    page change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true // Set to true for a permanent redirect (301)
+      }
+    ];
+  }
+}
 
 export default nextConfig;
