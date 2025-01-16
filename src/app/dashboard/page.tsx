08bc6a5d6c271cc7c2e6b71b37b7e64781ce07e2
Murli Patel <patelmurli15@gmail.com>
2025-01-16 17:51:59 +0530

    page change

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import TodoList from "@/components/TodoList";
+import axios from "axios";
+import { Suspense } from "react";
+const Dashboard = () => {
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("api/users/logout");
+            router.push("/login");
+        } catch (error) {
+            console.log("Error logging out:", error);
+        }
+    };
+
+    return (
+        <>
+		<Suspense fallback={<div>Loading...</div>}>
+            <main className="p-5 inset-0 mx-auto md:w-[75vh]">
+                <div className="flex justify-end p-2">
+                    <button onClick={logout} className="bg-red-600 rounded-md p-2">
+                        Logout
+                    </button>
+                </div>
+                <h1 className="font-bold text-2xl flex flex-col justify-center items-center border-b my-5 py-5 border-gray-600 ">
+                    TODO APP
+                </h1>
+                <Navbar />
+                <TodoList />
+            </main>
+			</Suspense>
+        </>
+    );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,15 @@
 "use client";
 import { useRouter } from "next/navigation";
-import Navbar from "@/components/Navbar";
-import TodoList from "@/components/TodoList";
-import axios from "axios";
-import { Suspense } from "react";
+
 const Page = () => {
     const router = useRouter();
-    const logout = async () => {
-        try {
-            await axios.get("api/users/logout");
-            router.push("/login");
-        } catch (error) {
-            console.log("Error logging out:", error);
-        }
-    };
-
+    const onLogin = () => {
+        router.push("/login");
+    }
     return (
         <>
-		<Suspense fallback={<div>Loading...</div>}>
-            <main className="p-5 inset-0 mx-auto md:w-[75vh]">
-                <div className="flex justify-end p-2">
-                    <button onClick={logout} className="bg-red-600 rounded-md p-2">
-                        Logout
-                    </button>
-                </div>
-                <h1 className="font-bold text-2xl flex flex-col justify-center items-center border-b my-5 py-5 border-gray-600 ">
-                    TODO APP
-                </h1>
-                <Navbar />
-                <TodoList />
-            </main>
-			</Suspense>
+		<h1>Home Page</h1>
+        <button onClick={onLogin}>Login Page</button>
         </>
     );
 };
